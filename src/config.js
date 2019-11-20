65493dc572d8d6ad9c8524a1c53790dd7dfb2eff
Steven Bull <stevenwbulljr@gmail.com>
2019-11-20 22:07:07 +0000

    add ternary operator to handle API url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: (process.env.NODE_ENV === 'production' 
+    ? 'https://minionese-stevenb-brock.herokuapp.com/api' : 'http://localhost:8000/api'),
   TOKEN_KEY: process.env.TOKEN_KEY || 'learning-client-auth-token',
 }
